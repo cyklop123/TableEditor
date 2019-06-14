51d08ec0b7c4305c6fe16d37caf596e28162e5b0
cyklop123 <irekbiela@gmail.com>
2019-06-14 22:02:47 +0200

    improve methods

--- src/main/java/sample/TableViewModel.java
+++ src/main/java/sample/TableViewModel.java
@@ -1,12 +1,18 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 
+import javax.xml.soap.Text;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class TableViewModel {
     private static List<ArrayList<String>> table = new ArrayList<ArrayList<String>>();
@@ -57,18 +63,68 @@ public class TableViewModel {
 
     public void addRow()
     {
-        HBox row = new HBox();
-        GridPane.setConstraints(row,0, UserInputWindowController.getRows()+1);
-        for(int i = 0; i< UserInputWindowController.getColumns(); i++)
-        {
-            TextField cell = new TextField();
-            cell.setPromptText("row "+(UserInputWindowController.getRows()+1)+" col "+(i+1));
-            cell.setMaxWidth(140);
-            cell.setMinWidth(40);
-            row.getChildren().addAll(cell);
-        }
-        container.getChildren().addAll(row);
-        UserInputWindowController.incrementRows();
+        TextInputDialog dialog = new TextInputDialog("0");
+        dialog.setTitle("Input Rows");
+        dialog.setHeaderText("Enter number of rows");
+        dialog.setContentText("Rows:");
+
+        //walidacja danych wejściowych
+        Button ok = (Button) dialog.getDialogPane().lookupButton(ButtonType.OK);
+        ok.addEventFilter(ActionEvent.ACTION, ae -> {
+            try{
+                int val = Integer.parseInt(dialog.getEditor().getText());
+                if(val < 0 || val > UserInputWindowController.getRows())
+                {
+                    throw new Exception("Bad data");
+                }
+            }
+            catch (NumberFormatException e)
+            {
+                ae.consume();
+            }
+            catch (Exception e)
+            {
+                ae.consume();
+            }
+        });
+
+        Optional<String> res = dialog.showAndWait();
+
+        res.ifPresent(val -> {
+
+            int index = Integer.parseInt(val);
+
+            HBox row = new HBox();
+            //ustawienie położenia
+            GridPane.setConstraints(row,0, index+1);
+            //przesunięcie poprzednich o jedno niżej
+            for(Node node: container.getChildren())
+            {
+                int i=container.getRowIndex(node);
+                if(i>index)
+                {
+                    container.setRowIndex(node,i+1);
+                    int j = 1;
+                    for (Node cell : ((HBox) (node)).getChildren()) {
+                        ((TextField) (cell)).setPromptText("row " + (i + 1) + " col" + j);
+                        j++;
+                    }
+                }
+            }
+            //tworzenie wiersza
+            for(int i = 0; i< UserInputWindowController.getColumns(); i++)
+            {
+                TextField cell = new TextField();
+                cell.setPromptText("row "+(index+1)+" col "+(i+1));
+                cell.setMaxWidth(140);
+                cell.setMinWidth(40);
+                row.getChildren().addAll(cell);
+            }
+            container.getChildren().add(index,row);
+            UserInputWindowController.incrementRows();
+            saveDataToArray();
+            System.out.println(table.toString());
+        });
     }
 
     public void addColumn()
